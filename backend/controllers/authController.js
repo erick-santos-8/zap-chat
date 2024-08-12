2f59fd764e69bb2c713e6c252f3f30a78caa0dde
erick-santos-8 <esagosto@hotmail.com>
2024-08-12 15:27:23 -0300

    Profile storage mongodb

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,8 +1,46 @@
+import User from "../models/userModel.js"
+
+
 export const signup = async (req, res) => {
     try{
         const {fullName, userName, password, confirmPassword, gender} = req.body;
+    
+    if(password !== confirmPassword){
+        return res.status(400).json({error:"Senhas diferentes!"})
+    }
+
+    const user = await User.findOne({userName});
+    if(user){
+        return res.status(400).json({error: "Usuário já existe."})
+    }
+
+    //HASH PASSWORD HERE
+
+    // https://avatar-placeholder.iran.liara.run/
+
+    const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${userName}`
+    const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${userName}`
+    
+    const newUser = new User({
+        fullName,
+        userName,
+        password,
+        gender,
+        profilePic: gender === "male"? boyProfilePic : girlProfilePic
+    })
+
+    await newUser.save();
+
+    res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        userName:newUser.userName,
+        profilePic:newUser.profilePic
+    })
+
     } catch (error) {
-        
+        console.log("Erro ao cadastrar", error.message);
+        res.status(500).json({error:"Erro interno"});
     }
 }
 
--- backend/server.js
+++ backend/server.js
@@ -5,18 +5,26 @@ import authRoutes from "./routes/authRoutes.js"
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 const app = express();
-dotenv.config();
-
 const PORT = process.env.PORT || 5000;
 
 
+dotenv.config();
 
 
-app.get("/", (req, res) =>{
-    res.send("Hello World!");
-})
+//Middlewares ---------------------------------
+//processa  os dados requisitados do req.body
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+//---------------------------------------------
+
+
+
+
+// app.get("/", (req, res) =>{
+//     res.send("Hello World!");
+// })
+
 
 app.listen(PORT, () => {
     connectToMongoDB();
