fe933c72eb94e0b855a1ff4a412087992836c8ae
erick-santos-8 <esagosto@hotmail.com>
2024-08-19 10:29:46 -0300

    logout function

--- frontend/src/pages/home/components/sidebar/components/LogoutButton.jsx
+++ frontend/src/pages/home/components/sidebar/components/LogoutButton.jsx
@@ -1,10 +1,16 @@
-import { Button } from '@/components/ui/button'
 import { LogOutIcon } from 'lucide-react'
+import useLogout from '@/hooks/useLogout'
 
 const LogoutButton = () => {
+
+  const {loading, logout } =  useLogout();
   return (
     <div className='mt-auto'>
-      <LogOutIcon className='w-6 h-6 cursor-pointer text-purple-950 hover:text-purple-900'/>
+      {!loading? (
+        <LogOutIcon className='w-6 h-6 cursor-pointer text-purple-950 hover:text-purple-900' onClick={logout}/>
+  ):(
+        <LogOutIcon className='w-6 h-6 cursor-wait text-gray-500'/>
+      )}
     </div>
   )
 }
